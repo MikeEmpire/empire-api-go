df9ee43dbc67de71947fcbe2bbd51ead3dafd068
Michael Olie <aolie94@gmail.com>
2024-06-06 21:24:39 -0700

    setup default route

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,16 @@ package routes
 import (
 	"empire-api-go/pkg/mail"
 	"empire-api-go/pkg/ptrains"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(r *gin.Engine) {
+	// Default route
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to the Empire API!"})
+	})
 	// Setup group routes
 	v1 := r.Group("/api/v1")
 	{
